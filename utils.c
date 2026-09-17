f16cb28daf619af2a47a902f2704b02865ecbd44
vfjpl <vfjpl1@gmail.com>
2026-09-17 21:37:50 +0200

    utils

--- utils.c
+++ utils.c
@@ -3,3 +3,30 @@
 #endif // _GNU_SOURCE
 
 #include "utils.h"
+#include <string.h>
+
+
+char* util_strcpy_p(char* dest, const char* src)
+{
+	return stpcpy(dest, src);//_GNU_SOURCE
+}
+size_t util_strcpy_l(char* dest, const char* src)
+{
+	return util_strcpy_p(dest, src) - dest;
+}
+void util_strscpy(char* dest, const char* src, size_t n)
+{
+	--n;
+	strncpy(dest, src, n)[n] = '\0';
+}
+char* util_strscpy_p(char* dest, const char* src, size_t n)
+{
+	--n;
+	char* end = stpncpy(dest, src, n);//_GNU_SOURCE
+	dest[n] = '\0';
+	return end;
+}
+size_t util_strscpy_l(char* dest, const char* src, size_t n)
+{
+	return util_strscpy_p(dest, src, n) - dest;
+}
--- utils.h
+++ utils.h
@@ -1,6 +1,14 @@
 #ifndef UTILS_H_INCLUDED
 #define UTILS_H_INCLUDED
 
+#include <sys/param.h>
+#include <stdbool.h>
+#include <stdint.h>
 
+char* util_strcpy_p(char* dest, const char* src);
+size_t util_strcpy_l(char* dest, const char* src);
+void util_strscpy(char* dest, const char* src, size_t n);
+char* util_strscpy_p(char* dest, const char* src, size_t n);
+size_t util_strscpy_l(char* dest, const char* src, size_t n);
 
 #endif // UTILS_H_INCLUDED
